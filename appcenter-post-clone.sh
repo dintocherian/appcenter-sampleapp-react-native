9e78a8ff352af631ff67fe39608359e786666e9b
Dinto Cherian <dintoambooken@gmail.com>
2020-04-04 23:10:16 +0530

    Update appcenter-post-clone.sh

--- appcenter-post-clone.sh
+++ appcenter-post-clone.sh
@@ -1,15 +1,11 @@
 #!/bin/bash -x
 
-cat /etc/os-release || echo true
-cat /etc/system-release || echo true
 aws --version || echo true
 uname -a || echo true
-free -h || echo true
-df -h || echo true
-cat /proc/cpuinfo || echo true
+
 
 brew install gnu-sed
-#PATH="/usr/local/opt/gnu-sed/libexec/gnubin:$PATH"
+PATH="/usr/local/opt/gnu-sed/libexec/gnubin:$PATH"
 
 sed -i "s/.*versionName.*$/versionName $VERSION/" $APPCENTER_SOURCE_DIRECTORY/android/app/build.gradle
 
