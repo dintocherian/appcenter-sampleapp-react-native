07925c0662e84921d3b9d247f5cded17dea9eeee
Dinto Cherian <dintoambooken@gmail.com>
2020-04-04 23:08:35 +0530

    Update appcenter-post-clone.sh

--- appcenter-post-clone.sh
+++ appcenter-post-clone.sh
@@ -9,9 +9,9 @@ df -h || echo true
 cat /proc/cpuinfo || echo true
 
 brew install gnu-sed
-PATH="/usr/local/opt/gnu-sed/libexec/gnubin:$PATH"
-ls -ltr /usr/local/opt/gnu-sed/libexec/gnubin/
-echo $PATH
+#PATH="/usr/local/opt/gnu-sed/libexec/gnubin:$PATH"
 
 sed -i "s/.*versionName.*$/versionName $VERSION/" $APPCENTER_SOURCE_DIRECTORY/android/app/build.gradle
-sed -i "s|.*MARKETING_VERSION.*$|<string>$VERSION</string>|" $APPCENTER_SOURCE_DIRECTORY/ios/Surge/Info.plist
+
+cat $APPCENTER_SOURCE_DIRECTORY/android/app/build.gradle
+#sed -i "s|.*MARKETING_VERSION.*$|<string>$VERSION</string>|" $APPCENTER_SOURCE_DIRECTORY/ios/Surge/Info.plist
