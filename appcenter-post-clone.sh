b613818a513f3a6a7ac00c8e2eb1c02c1711cb28
Dinto Cherian <dintoambooken@gmail.com>
2020-05-29 18:10:01 +0530

    Update appcenter-post-clone.sh

--- appcenter-post-clone.sh
+++ appcenter-post-clone.sh
@@ -15,4 +15,17 @@ echo PODS_ROOT $PODS_ROOT || echo true
 echo PROJECT_DIR $PROJECT_DIR || echo true
 echo DWARF_DSYM_FOLDER_PATH $DWARF_DSYM_FOLDER_PATH || echo true
 echo DWARF_DSYM_FILE_NAME $DWARF_DSYM_FILE_NAME || echo true
+
+echo APPCENTER_ANDROID_VARIANT $APPCENTER_ANDROID_VARIANT || echo true
+echo APPCENTER_ANDROID_MODULE $APPCENTER_ANDROID_MODULE || echo true
+
+ if [ -z "$APPCENTER_ANDROID_MODULE" ] 
+    then
+        echo "Not an Android Build!"
+    else
+        echo "Its Android build"
+    fi
+    
+    
 exit 1
+
